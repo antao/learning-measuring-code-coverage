9f8b696c05ccbd5b59c2e7e31cdd524f1224602d
Joao Antao <joao.antao@gmail.com>
2020-04-04 23:56:23 +0200

    Add library (.NET Core 3.1) example and unit test project (xUnit) to measure the code coverage with Coverlet

---
+++ src/coverlet-library-sample/Calculator.cs
@@ -0,0 +1,38 @@
+﻿namespace coverlet_library_sample
+{
+    public sealed class Calculator
+    {
+        /// <summary>
+        /// Division method represents the breaking of x into an equal y of parts, it is not commutative and is a basic arithmetic operation.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public decimal Division(decimal x, decimal y)
+        {
+            return x / y;
+        }
+
+        /// <summary>
+        /// Addition method represents the sum of x and y.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public decimal Addition(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        /// <summary>
+        /// Subtraction method represents the difference of x and y, the inverse of addition.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public decimal Subraction(decimal x, decimal y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ tests/coverlet-library-unittests/CalculatorTests.cs
@@ -0,0 +1,64 @@
+using System;
+using coverlet_library_sample;
+using Xunit;
+
+namespace coverlet_libraby_unittests
+{
+    public class CalculatorTests
+    {
+        private readonly Calculator _target;
+
+        public CalculatorTests()
+        {
+            _target = new Calculator();
+        }
+
+        [Theory]
+        [InlineData(1, 1, 1)]
+        [InlineData(10, 1, 10)]
+        [InlineData(100, 20, 5)]
+        public void Division(decimal x, decimal y, decimal expected)
+        {
+            // Arrange
+            // Act
+            var actual = _target.Division(x, y);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Division_Should_Throw_DivideByZeroException_When_Dividing_By_Zero()
+        {
+            Assert.Throws<DivideByZeroException>(() => _target.Division(42, 0));
+        }
+
+        [Theory]
+        [InlineData(41, 1, 42)]
+        [InlineData(0, 42, 42)]
+        [InlineData(42, 0, 42)]
+        public void Addition(decimal x, decimal y, decimal expected)
+        {
+            // Arrange
+            // Act
+            var actual = _target.Addition(x, y);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(43, 1, 42)]
+        [InlineData(0, 42, -42)]
+        [InlineData(42, 0, 42)]
+        public void Subtraction(decimal x, decimal y, decimal expected)
+        {
+            // Arrange
+            // Act
+            var actual = _target.Subraction(x, y);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
